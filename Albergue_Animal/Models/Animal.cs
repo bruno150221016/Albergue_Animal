7c0158419bf1796a2cb00ee9540e90829677f30c
Inês Norberto <150221061@estudantes.ips.pt>
2018-12-08 15:18:11 +1300

    alterar os nomes das tabelas

--- Albergue_Animal/Models/Animal.cs
+++ Albergue_Animal/Models/Animal.cs
@@ -8,19 +8,23 @@ namespace Albergue_Animal.Models
 {
     public class Animal
     {
-
+        [Display(Name = "ID")]
         public int AnimalId { get; set; }
 
         public String Nome { get; set; }
 
+        [Display(Name = "Raça")]
         public String Raca { get; set; }
 
+        [Display(Name = "Género")]
         public String Sexo { get; set; }
 
         public String Cor { get; set; }
 
+        [Display(Name = "Data Entrada")]
         public DateTime DataEntrada { get; set; }
 
+        [Display(Name = "Data Vacina")]
         public DateTime DataVacina { get; set; }
 
         [StringLength(255)]
